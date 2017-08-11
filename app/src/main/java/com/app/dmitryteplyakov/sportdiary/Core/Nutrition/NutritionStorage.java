5698c8e35b0179c3ac5ddbc0830f1f98486a86db
Dmitry Teplyakov <dimon2242@gmail.com>
2017-08-11 23:44:09 +0300

    Add NutrtitonStorage to Core

---
+++ app/src/main/java/com/app/dmitryteplyakov/sportdiary/Core/Nutrition/NutritionStorage.java
@@ -0,0 +1,158 @@
+package com.app.dmitryteplyakov.sportdiary.Core.Nutrition;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.app.dmitryteplyakov.sportdiary.database.Nutrition.NutritionBaseHelper;
+import com.app.dmitryteplyakov.sportdiary.database.Nutrition.NutritionCursorWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static com.app.dmitryteplyakov.sportdiary.database.Nutrition.NutritionDbSchema.*;
+
+/**
+ * Created by dmitry21 on 11.08.17.
+ */
+
+public class NutritionStorage {
+    private static NutritionStorage sNutritionStorage;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    public static NutritionStorage get(Context context) {
+        if(sNutritionStorage == null)
+            sNutritionStorage = new NutritionStorage(context);
+        return sNutritionStorage;
+    }
+
+    private NutritionStorage(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new NutritionBaseHelper(mContext).getWritableDatabase();
+    }
+
+    private static ContentValues getContentValues(Nutrition nutrition) {
+        ContentValues values = new ContentValues();
+        values.put(NutritionTable.Cols.UUID, nutrition.getId().toString());
+        values.put(NutritionTable.Cols.TITLE, nutrition.getProductTitle());
+        values.put(NutritionTable.Cols.PARENTDAYUUID, nutrition.getParentDay().toString());
+        values.put(NutritionTable.Cols.ASSOCIATEDDAYUUID, nutrition.getAssociatedDay().toString());
+        values.put(NutritionTable.Cols.ENERGY, nutrition.getEnergy());
+        values.put(NutritionTable.Cols.ISASSOCIATEDWITHDAY, nutrition.isAssociatedWithDay() ? 1 : 0);
+        values.put(NutritionTable.Cols.WEIGHT, nutrition.getWeight());
+        return values;
+    }
+
+    private NutritionCursorWrapper queryNutritions(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                NutritionTable.NAME,
+                null, // Columns - null перебирает все столбцы
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new NutritionCursorWrapper(cursor);
+    }
+
+    public List<Nutrition> getNutritions() {
+        List<Nutrition> nutritions = new ArrayList<>();
+
+        NutritionCursorWrapper cursor = queryNutritions(null, null);
+        try {
+            cursor.moveToFirst();
+            while(!cursor.isAfterLast()) {
+                nutritions.add(cursor.getNutrition());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return nutritions;
+    }
+
+    public List<Nutrition> getNutritionsByParentDayId(UUID parentId) {
+        List<Nutrition> nutritions = new ArrayList<>();
+        NutritionCursorWrapper cursor = queryNutritions(NutritionTable.Cols.PARENTDAYUUID + " = ?",
+                new String[] { parentId.toString() }
+        );
+
+        try {
+            cursor.moveToFirst();
+            while(!cursor.isAfterLast()) {
+                nutritions.add(cursor.getNutrition());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return nutritions;
+    }
+
+    public List<Nutrition> getNutritionsByAssociatedDayId(UUID associatedId) {
+        List<Nutrition> nutritions = new ArrayList<>();
+
+        NutritionCursorWrapper cursor = queryNutritions(NutritionTable.Cols.ASSOCIATEDDAYUUID + " = ?",
+                new String[] { associatedId.toString() }
+        );
+
+        cursor.moveToFirst();
+
+        try {
+            while(!cursor.isAfterLast()) {
+                nutritions.add(cursor.getNutrition());;
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return nutritions;
+    }
+
+    public Nutrition getNutrition(UUID id) {
+        NutritionCursorWrapper cursor = queryNutritions(NutritionTable.Cols.UUID + " = ?",
+                new String[] { id.toString() }
+        );
+
+        try {
+            if(cursor.getCount() == 0)  return null;
+            cursor.moveToFirst();
+            return cursor.getNutrition();
+        } finally {
+            cursor.close();
+        }
+    }
+
+    public void addNutrition(Nutrition nutrition) {
+        ContentValues values = getContentValues(nutrition);
+        mDatabase.insert(NutritionTable.NAME, null, values);
+    }
+
+    public void updateNutrition(Nutrition nutrition) {
+        String uuidString = nutrition.getId().toString();
+        ContentValues values = getContentValues(nutrition);
+        mDatabase.update(NutritionTable.NAME, values, NutritionTable.Cols.UUID + " = ?",
+                new String[] { uuidString }
+        );
+    }
+
+    public void deleteNutrition(Nutrition nutrition) {
+        mDatabase.delete(NutritionTable.NAME, NutritionTable.Cols.UUID + " = ?",
+                new String[] { nutrition.getId().toString() }
+        );
+    }
+
+    public void deleteNutritionsByParentDayId(UUID parentId) {
+        mDatabase.delete(NutritionTable.NAME, NutritionTable.Cols.PARENTDAYUUID + " = ?",
+                new String[] { parentId.toString() }
+        );
+    }
+
+}
