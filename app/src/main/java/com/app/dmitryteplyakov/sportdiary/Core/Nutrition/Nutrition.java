5ac5565af73471dd61a6eda03e4aeb99f88a1758
Dmitry Teplyakov <dimon2242@gmail.com>
2017-08-14 20:39:03 +0300

    Change double to int type for nutrition weight

--- app/src/main/java/com/app/dmitryteplyakov/sportdiary/Core/Nutrition/Nutrition.java
+++ app/src/main/java/com/app/dmitryteplyakov/sportdiary/Core/Nutrition/Nutrition.java
@@ -11,7 +11,7 @@ public class Nutrition {
     private UUID mId;
     private UUID mParentDay;
     private int mEnergy;
-    private double mWeight;
+    private int mWeight;
 
 
     public Nutrition(UUID mId, UUID mParentDay) {
@@ -51,11 +51,18 @@ public class Nutrition {
         mEnergy = energy;
     }
 
-    public double getWeight() {
+    public int getWeight() {
         return mWeight;
     }
 
-    public void setWeight(double weight) {
+    public void setWeight(int weight) {
         mWeight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this.getId().equals(((Nutrition) o).getId()))
+            return true;
+        return false;
     }
 }
