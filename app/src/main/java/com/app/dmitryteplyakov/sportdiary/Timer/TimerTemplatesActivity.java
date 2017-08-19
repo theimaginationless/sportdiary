4f10e1e720b0bba5e50b969debf25b55d5836a65
Dmitry Teplyakov <dimon2242@gmail.com>
2017-08-19 10:54:33 +0300

    Drawer layouts and menus. Initial Timer Templates

---
+++ app/src/main/java/com/app/dmitryteplyakov/sportdiary/Timer/TimerTemplatesActivity.java
@@ -0,0 +1,10 @@
+package com.app.dmitryteplyakov.sportdiary.Timer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dmitry21 on 19.08.17.
+ */
+
+public class TimerTemplatesActivity extends AppCompatActivity {
+}
