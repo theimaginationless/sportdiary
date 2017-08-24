f08a5bf8d55c5084383f3c87ee384c438c2e3d4b
Dmitry Teplyakov <dimon2242@gmail.com>
2017-08-24 19:07:46 +0300

    Timer List fragment init

--- app/src/main/java/com/app/dmitryteplyakov/sportdiary/Timer/TimerListActivity.java
+++ app/src/main/java/com/app/dmitryteplyakov/sportdiary/Timer/TimerListActivity.java
@@ -2,7 +2,13 @@ package com.app.dmitryteplyakov.sportdiary.Timer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.app.dmitryteplyakov.sportdiary.R;
 
 import java.util.UUID;
 
@@ -17,6 +23,32 @@ public class TimerListActivity extends AppCompatActivity {
         Intent intent = new Intent(context, TimerListActivity.class);
         intent.putExtra(EXTRA_TEMPLATE_UUID, id);
         return intent;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programs_common);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.programs_fragment_container);
+        if(fragment == null) {
+            fragment = TimerListFragment.newInstance((UUID) getIntent().getSerializableExtra(EXTRA_TEMPLATE_UUID));
+            fm.beginTransaction()
+                    .add(R.id.common_fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/app/dmitryteplyakov/sportdiary/Timer/TimerListFragment.java
+++ app/src/main/java/com/app/dmitryteplyakov/sportdiary/Timer/TimerListFragment.java
@@ -1,10 +1,72 @@
 package com.app.dmitryteplyakov.sportdiary.Timer;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.app.dmitryteplyakov.sportdiary.Core.Timer.Timer;
+import com.app.dmitryteplyakov.sportdiary.R;
+
+import java.util.UUID;
 
 /**
  * Created by dmitry21 on 24.08.17.
  */
 
 public class TimerListFragment extends Fragment {
+    private static final String ARG_TEMPLATE_UUID = "com.app.timerlistfragment.arg.template.uuid";
+    private RecyclerView mRecyclerView;
+
+    public static TimerListFragment newInstance(UUID id) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_TEMPLATE_UUID, id);
+        TimerListFragment fragment = new TimerListFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_list_programs, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_list_programs_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return v;
+    }
+
+    private class TimerHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        private Timer mTimer;
+        private TextView mTimeSequence;
+        private TextView mTitle;
+
+        public TimerHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+            mTitle = (TextView) itemView.findViewById(R.id.timer_name);
+            mTimeSequence = (TextView) itemView.findViewById(R.id.time_sequence_item);
+        }
+
+        public void bindTimer(Timer timer) {
+            mTimer = timer;
+            mTitle.setText(mTimer.getTitle());
+            mTimeSequence.setText(Integer.toString(mTimer.getPreparing()) + "-" + Integer.toString(mTimer.getWorkout()) + "-" + Integer.toString(mTimer.getRest()) + "-" + Integer.toString(mTimer.getCalmDown()));
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+
+            return true;
+        }
+    }
 }
